b273033f3c84d6f22f497f360669c0ebfa1f8d74
Rupert Redington <rupert@neontribe.co.uk>
2013-04-24 13:04:38 +0100

    Async loader and demo page

---
+++ app/lmi-everywhere.js
@@ -0,0 +1,45 @@
+(function(){
+    function getScriptUrl() {
+        var scripts = document.getElementsByTagName('script');
+        var element;
+        var src;
+        for (var i = 0; i < scripts.length; i++) {
+            element = scripts[i];
+            src = element.src;
+
+            if (src && /lmi-everywhere\.js/.test(src)) {
+                return src;
+            }
+        }
+        return null;
+    }
+
+    function getQueryParameters(query) {
+        var args = query.split('&'),
+            params = {},
+            pair,
+            key,
+            value;
+        function decode(string) {
+            return decodeURIComponent(string || '').replace('+', ' ');
+        }
+        for (var i = 0; i < args.length; i++) {
+            pair = args[i].split('=');
+            key = decode(pair[0]);
+            value = decode(pair[1]);
+            params[key] = value;
+        }
+        return params;
+    }
+
+    var url = getScriptUrl();
+    var params = getQueryParameters(url.replace(/^.*\?/, ''));
+    var iframe = document.createElement('iframe');
+    var target = document.getElementsByTagName('script')[0]
+
+    iframe.src = "./index.html?" + url.replace(/^.*\?/, '');
+    iframe.style.width = params.width || '250px';
+    iframe.style.height = params.height || '300px';
+    iframe.style.border = 'none';
+    target.parentNode.insertBefore(iframe, target);
+})();
