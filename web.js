5c98d4544c0da9a8c7d171a366dbf5f73364c6d2
Rupert Redington <rupert@neontribe.co.uk>
2013-06-04 10:12:01 +0100

    Heroku deployment

---
+++ web.js
@@ -0,0 +1,9 @@
+var connect = require("connect"),
+  port = process.env.PORT || 3000,
+  
+connect(
+  connect.logger(),
+  connect.static(__dirname + "/app")
+).listen(port);
+
+console.log('lmi-everywhere server running on port ' + port);
